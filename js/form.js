7ef9fb7dbbce8d3971ba4d09b8372c1e6919cc6b
mxhsb <mxcoder@yandex.com>
2018-10-03 18:17:10 -0300

    ajustes fomulario

--- js/form.js
+++ js/form.js
@@ -2,77 +2,63 @@
 var config = {
 	apiKey: "AIzaSyCbPn7WVhHjPVDLzH7Wd-EDesXINZe7_tg",
 	authDomain: "evolvemidia-4ad05.firebaseapp.com",
-	// databaseURL: "https://evolvemidia-4ad05.firebaseio.com",
+	databaseURL: "https://evolvemidia-4ad05.firebaseio.com",
 	projectId: "evolvemidia-4ad05",
-	// storageBucket: "evolvemidia-4ad05.appspot.com",
-	// messagingSenderId: "796346727409"
+	storageBucket: "evolvemidia-4ad05.appspot.com",
+	messagingSenderId: "796346727409"
 };
 firebase.initializeApp(config);
-
-const firebase = require('firebase');
-require('firebase/firestore');
-
 var db = firebase.firestore();
+
 db.settings({
 	timestampsInSnapshots: true
-})
-
-
-// Salvar dados do usuário
-function salvarForm() {
-	var email = document.getElementById('email').value;;
-	if (email.search('@') === -1) {
-
-		document.querySelector(".alerta").style.display = "block";
-		return false
-	}
-
-	var nome = document.getElementById('nome').value;
-	var empresa = document.getElementById('empresa').value;;
-	
-	var d = new Date();
-	var dia = ("0" + (d.getDate() +1)).slice(-2);
-	var mes = ("0" + (d.getMonth() +1)).slice(-2);
-	var data = d.getFullYear() +'-'+ mes +'-'+ dia;
-
-	var atual = new Date();
-	var diferenca = -(atual.getTimezoneOffset()/60);
-	var hora = new Date( new Date().getTime() + diferenca * 3600 * 1000).toUTCString().replace( / GMT$/, "" );
-	hora = hora.slice(-8);
-
-	var data_hora = data + ' ' + hora;
-
-	return false
-};
-
-// Salvar endereco ip
-$.getJSON('http://gd.geobytes.com/GetCityDetails?callback=?', function(data) {
-	var resposta = JSON.stringify(data, null, 2);
-	var ip = resposta['geobytesipaddress'];
-	return false
 });
 
-
-function enviarForm(email, nome, ip, empresa, data_hora) {
-	var objeto = {
-		email: email,
-		nome: nome,
-		ip: ip,
-		empresa: empresa,
-		data_hora: data_hora
-	}
-	db.collection('captura-leads').set(objeto)
-	return false
-}
+var ref = db.collection('leads').doc();
 
 // Esperar o carregamento completo da página antes de qualquer coisa
 document.addEventListener('DOMContentLoaded', function() {
 
 	// Executar envio ao bando de dados quando o usuário pressionar o botão
-	document.querySelector("#captura").onclick = 
-	enviarForm(email, nome, ip, empresa, data_hora);
+	document.querySelector("#captura").onclick = function() {
 
+		var email = document.getElementById('email').value;
+		var nome = document.getElementById('nome').value;
+		var tipo = document.getElementById('tipo').value;
+		
+		var d = new Date();
+		var dia = ("0" + (d.getDate() +1)).slice(-2);
+		var mes = ("0" + (d.getMonth() +1)).slice(-2);
+		var data = d.getFullYear() +'-'+ mes +'-'+ dia;
+	
+		var atual = new Date();
+		var diferenca = -(atual.getTimezoneOffset()/60);
+		var hora = new Date( new Date().getTime() + diferenca * 3600 * 1000).toUTCString().replace( / GMT$/, "" );
+		hora = hora.slice(-8);
+	
+		var data_hora = data + ' ' + hora;
+		var ip = "";
+	
+		// Salvar endereco ip
+		$.getJSON('https://jsonip.com/?callback=?', function(data) {
+			ip = data.ip;
+
+			// Enviar para o banco de dados
+			var envio = ref.set({
+				email: email,
+				nome: nome,
+				ip: ip,
+				tipo: tipo,
+				data_hora: data_hora
+			});
+		
+			if (nome === '' || email === '') {
+				document.querySelector(".alerta2").style.display = "block"
+				return false;
+			}
+			// Mostrar mensagem ao usuário que os dados foram enviados
+			document.querySelector(".alerta1").style.display = "block"
+			return false;
+		});
+	}
 });
-
-// <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
-// <script src="https://www.gstatic.com/firebasejs/5.5.2/firebase.js"></script>
