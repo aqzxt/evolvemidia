47a540cdf755de66f39f146545a7d965a683e0e6
mxhsb <mxcoder@yandex.com>
2018-10-04 15:13:47 -0300

    melhorias

--- js/form.js
+++ js/form.js
@@ -14,17 +14,21 @@ db.settings({
 	timestampsInSnapshots: true
 });
 
+// Referenciar db "leads" no firestore
 var ref = db.collection('leads').doc();
 
 // Esperar o carregamento completo da página antes de qualquer coisa
 document.addEventListener('DOMContentLoaded', function() {
 
-	// Executar envio ao bando de dados quando o usuário pressionar o botão
+	// Executar envio ao banco de dados quando o usuário pressionar o botão
 	document.querySelector("#captura").onclick = function() {
 
 		var email = document.getElementById('email').value;
 		var nome = document.getElementById('nome').value;
-		var tipo = document.getElementById('tipo').value;
+
+		// var dominios = []
+
+		var tipo = "B2C";
 		
 		var d = new Date();
 		var dia = ("0" + (d.getDate() +1)).slice(-2);
@@ -43,22 +47,24 @@ document.addEventListener('DOMContentLoaded', function() {
 		$.getJSON('https://jsonip.com/?callback=?', function(data) {
 			ip = data.ip;
 
-			// Enviar para o banco de dados
-			var envio = ref.set({
-				email: email,
-				nome: nome,
-				ip: ip,
-				tipo: tipo,
-				data_hora: data_hora
-			});
-		
-			if (nome === '' || email === '') {
-				document.querySelector(".alerta2").style.display = "block"
+			if (nome.length > 2 && email.length > 4) {
+				// Enviar para o banco de dados
+				var envio = ref.set({
+					email: email,
+					nome: nome,
+					ip: ip,
+					tipo: tipo,
+					data_hora: data_hora
+				});
+				document.querySelector(".box-button").style.display = "none";
+				document.querySelector(".alerta").style.display = "block";
+				document.querySelector(".form-input").value = "";
+				document.querySelector(".f2").value = "";
 				return false;
 			}
-			// Mostrar mensagem ao usuário que os dados foram enviados
-			document.querySelector(".alerta1").style.display = "block"
+
 			return false;
 		});
 	}
+	return false
 });
